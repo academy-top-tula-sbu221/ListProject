68f573c21ef2aad58a36d99adc9cefeb717fea51
maxefimov <efimov.academy.top@gmail.com>
2023-03-04 13:23:45 +0300

    Добавьте файлы проекта.

---
+++ ListProject/ListProject.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct DArray
+{
+    int* array;
+    int size;
+};
+
+
+struct Node
+{
+    int value;
+    Node* next;
+};
+
+void Push(Node*& head, int value);
+int Pop(Node*& head);
+
+void List(Node* head);
+
+int main()
+{
+    Node* head = nullptr;
+    Push(head, 100);
+    Push(head, 200);
+    Push(head, 300);
+
+    List(head);
+}
+
+
+
+void Push(Node*& head, int value)
+{
+    Node* nodeNew = new Node;
+    nodeNew->value = value;
+    nodeNew->next = head;
+    head = nodeNew;
+}
+
+int Pop(Node*& head)
+{
+    Node* nodeDel = head;
+    int value = head->value;
+    head = head->next;
+    delete nodeDel;
+
+    return value;
+}
+
+
+void List(Node* head)
+{
+    while (head)
+    {
+        cout << head->value << "\n";
+        head = head->next;
+    }
+}
